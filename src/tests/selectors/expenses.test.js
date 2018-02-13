8220d26766f5574beba0e4efa3e609bf25952053
Matthew Smith <mas012691@gmail.com>
2018-02-13 16:22:29 -0500

    Tests through AddExpensePage

--- src/tests/selectors/expenses.test.js
+++ src/tests/selectors/expenses.test.js
@@ -1,6 +1,6 @@
-import expenses from '../fixtures/expenses'
 import moment from 'moment';
 import selectExpenses from '../../selectors/expenses';
+import expenses from '../fixtures/expenses';
 
 test('should filter by text value', () => {
   const filters = {
