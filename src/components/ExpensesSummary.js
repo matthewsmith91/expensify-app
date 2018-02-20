d3be2d235989b349985c4a2e86b3340eb13efcf3
Matthew Smith <mas012691@gmail.com>
2018-02-20 13:43:52 -0500

    Setup test database environment

--- src/components/ExpensesSummary.js
+++ src/components/ExpensesSummary.js
@@ -1,21 +1,27 @@
-import React from 'react'
-import {connect} from 'react-redux'
-import expensesTotal from '../selectors/expenses-total'
+import React from 'react';
+import { connect } from 'react-redux';
+import numeral from 'numeral';
+import selectExpenses from '../selectors/expenses';
+import selectExpensesTotal from '../selectors/expenses-total';
 
-export class ExpensesSummary extends React.Component {
-    render() {
-        return (
-            <div>
-                <p>Viewing {this.props.expenses ? this.props.length : 0} expenses totaling {expensesTotal(this.props.expenses)}</p>
-            </div>
-        )
-    }
-}
+export const ExpensesSummary = ({ expenseCount, expensesTotal }) => {
+  const expenseWord = expenseCount === 1 ? 'expense' : 'expenses' ;
+  const formattedExpensesTotal = numeral(expensesTotal / 100).format('$0,0.00');
+  
+  return (
+    <div>
+      <h1>Viewing {expenseCount} {expenseWord} totalling {formattedExpensesTotal}</h1>
+    </div>
+  );
+};
 
 const mapStateToProps = (state) => {
-    return {
-        expenses: state.expenses
-    }
-}
+  const visibleExpenses = selectExpenses(state.expenses, state.filters);
 
-export default connect()(ExpensesSummary)
+  return {
+    expenseCount: visibleExpenses.length,
+    expensesTotal: selectExpensesTotal(visibleExpenses)
+  };
+};
+
+export default connect(mapStateToProps)(ExpensesSummary);
